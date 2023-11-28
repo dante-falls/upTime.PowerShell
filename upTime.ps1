63e08e960fbb7fecbd06cc2af81ae8043d011657
Dante Falls <dante.falls@yahoo.com>
2023-11-27 17:08:00 -0800

    Update upTime.ps1

--- upTime.ps1
+++ upTime.ps1
@@ -1,5 +1,5 @@
 #This script retrieves the uptime of a remote computer.
 
-$RemoteComputerName = "DanteCLIENT01"
+$RemoteComputerName = "REMOTECOMPUTER" #Replace this value with the name of the remote computer you want to test.
 
 Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $RemoteComputerName | Select-Object LastBootUpTime
